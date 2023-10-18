42a1f917ecc8c2f91ddf9ba6b4f06fb7e032c588
ImChang-gyu <chang.gyu@illunex.com>
2023-10-18 17:28:42 +0900

    update

--- plugin29.js
+++ plugin29.js
@@ -20,7 +20,7 @@
         var e = t.contentDocument || t.contentWindow.document;
         e.open(),
           e.write(
-            '<!DOCTYPE html><script async type="module" src="https://cdn.jsdelivr.net/gh/ImChang-gyu/sdk-deploy-test/dist20/assets/index-87c46da6.js" charset="UTF-8"></script>'
+            '<!DOCTYPE html><script async type="module" src="https://cdn.jsdelivr.net/gh/ImChang-gyu/sdk-deploy-test/dist21/assets/index-10fbcf6c.js" charset="UTF-8"></script>'
           ),
           e.write(
             '<html lang="ko"><head><meta charset="utf-8"></head><body><div id="root"></div></body></html>'
