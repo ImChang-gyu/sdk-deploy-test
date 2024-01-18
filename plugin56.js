c6aa69eec82a794996e03cdf3d876c5444becbaf
ImChang-gyu <chang.gyu@illunex.com>
2024-01-18 17:09:16 +0900

    update

--- plugin56.js
+++ plugin56.js
@@ -20,7 +20,7 @@
         var e = t.contentDocument || t.contentWindow.document;
         e.open(),
           e.write(
-            '<!DOCTYPE html><script async type="module" src="https://cdn.jsdelivr.net/gh/ImChang-gyu/sdk-deploy-test/index-f63426eb.js" charset="UTF-8"></script><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ImChang-gyu/sdk-deploy-test/index-9825e6cc.css">'
+            '<!DOCTYPE html><script async type="module" src="https://cdn.jsdelivr.net/gh/ImChang-gyu/sdk-deploy-test/index-193f7c05.js" charset="UTF-8"></script><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ImChang-gyu/sdk-deploy-test/index-3715e62e.css">'
           ),
           e.write(
             '<html lang="ko"><head><meta charset="utf-8"></head><body><div id="root"></div></body></html>'
