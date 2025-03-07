5d0b6951f62542c412d0270b41b115ad5dc35e81
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2025-03-07 16:28:33 +0900

    update sdk

--- plugin60.js
+++ plugin60.js
@@ -6,21 +6,21 @@
       e
     ) && window.navigator.cookieEnabled)
   ) {
-    if (!document.getElementById("ntbot-plugin")) {
-      var n = document.createElement("div");
-      (n.id = "ntbot-plugin"), document.body.appendChild(n);
+    if (!document.getElementById('ntbot-plugin')) {
+      var n = document.createElement('div');
+      (n.id = 'ntbot-plugin'), document.body.appendChild(n);
     }
-    var i = document.getElementById("ntbot-plugin");
+    var i = document.getElementById('ntbot-plugin');
     i.style.zIndex = 1000000;
     i.innerHTML +=
       '<style>.hidden{ visibility: hidden; } @media (max-width:444px) { #ntbot-plugin-script{ width:100%!important;height:calc(100% - 110px)!important;right:0px!important; }}</style><div class="ntbot-entry"></div><div id="ntbot-plugin-script" class="hidden" style="position:fixed!important;right:24px;bottom:110px!important;width:420px;min-height:200px;max-height:750px;height: calc(100% - 110px);" ><iframe id="ntbot-script-iframe" title="Ntbot" style="position:relative!important;height:100%!important;width:100%!important;border:none!important;"></iframe></div>';
-    var t = document.getElementById("ntbot-script-iframe"),
+    var t = document.getElementById('ntbot-script-iframe'),
       r = !1,
       o = function () {
         var e = t.contentDocument || t.contentWindow.document;
         e.open(),
           e.write(
-            '<!DOCTYPE html><script async type="module" src="https://cdn.jsdelivr.net/gh/ImChang-gyu/sdk-deploy-test/index-f38401d6.js" charset="UTF-8"></script><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ImChang-gyu/sdk-deploy-test/index-3715e62e.css">'
+            '<!DOCTYPE html><script async type="module" src="https://cdn.jsdelivr.net/gh/ImChang-gyu/sdk-deploy-test/index-13925c07.js" charset="UTF-8"></script><link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/ImChang-gyu/sdk-deploy-test/index-3715e62e.css">'
           ),
           e.write(
             '<html lang="ko"><head><meta charset="utf-8"></head><body><div id="root"></div></body></html>'
